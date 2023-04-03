fb7abf4d4a06a6871bc3904f26769358c4f7ab8b
Hussam Ali <hussamali30@gmail.com>
2023-04-03 15:29:55 +0200

    Adding GPIO Support

--- trial5.c
+++ trial5.c
@@ -3,21 +3,17 @@
 #include <linux/moduleparam.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
-#include <linux/gpio.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Hussam ");
-MODULE_DESCRIPTION("A hello world Pseudo device driver"); 
+MODULE_DESCRIPTION("A hello world Pseudo device driver");
 
 #define SIZE 15
-#define LED_PIN 2
-
 dev_t device_number;
 struct cdev st_characterDevice;
 struct class *myClass;
 struct device *myDevice;
-static unsigned char buffer[SIZE]="";
-
+static unsigned char buffer[SIZE] = "";
 
 // echo "hello world from Egypt" > /dev/test_file --> writes hello in buffer
 ssize_t driver_write(struct file *file, const char __user *userBuffer, size_t count, loff_t *offset)
@@ -32,66 +28,50 @@ ssize_t driver_write(struct file *file, const char __user *userBuffer, size_t co
     printk("%s: the offset %lld\n", __func__, *offset);
     /* we need to check if the user wants more that the size or not */
     /* This if condition guarantees that the count never exceeds the size of the buffer */
-    if(count + (*offset) > SIZE)
+    if (count + (*offset) > SIZE)
     {
         count = SIZE - (*offset);
     }
-    if(!count) // no more memory space left to guarantee that user won't write in a space outside the buffer space
+    if (!count) // no more memory space left to guarantee that user won't write in a space outside the buffer space
     {
         printk("No space left");
         return -1;
     }
     /* Returns number of not copied bytes */
     not_copied = copy_from_user(&buffer[*offset], userBuffer, count);
-    /* Handling GPIO */
-    /* According to the value that user has written, we set the gpio pin */
-    switch(buffer[0])
-    {
-        case '1':
-        gpio_set_value(LED_PIN, 1);
-        break;
-        case '0':
-        gpio_set_value(LED_PIN, 0);
-        break;
-    }
-    if(not_copied)
+    if (not_copied)
     {
         return -1;
     }
-    *offset = count; //update offset to ensure that it starts reading from the last known location
+    *offset = count; // update offset to ensure that it starts reading from the last known location
     printk("%s: number of written bytes: %ld\n", __func__, count);
     printk("%s: message: %s\n", __func__, buffer);
     return count; // return number of successfully read bytes
 }
 
-
-
-
 ssize_t driver_read(struct file *file, char __user *userBuffer, size_t count, loff_t *offset)
 {
     int not_copied;
-    //printk("Driver read is called right now\n");
+    // printk("Driver read is called right now\n");
     printk("%s: the count to read %ld\n", __func__, count);
     printk("%s: the offset %lld\n", __func__, *offset);
     /* we need to check if the user wants more that the size or not */
     /* This if condition guarantees that the count never exceeds the size of the buffer */
-    if(count + (*offset) > SIZE)
+    if (count + (*offset) > SIZE)
     {
         count = SIZE - (*offset);
     }
     /* Returns number of not copied bytes */
     not_copied = copy_to_user(userBuffer, &buffer[*offset], count);
-    if(not_copied)
+    if (not_copied)
     {
         return -1;
     }
-    *offset = count; //update offset to ensure that it starts reading from the last known location
+    *offset = count; // update offset to ensure that it starts reading from the last known location
     printk("%s: number of not copied bytes: %d\n", __func__, not_copied);
     printk("%s: message: %s\n", __func__, userBuffer);
     return count; // return number of successfully read bytes
 }
-
-
 
 static int driver_open(struct inode *device_file, struct file *instance)
 {
@@ -101,22 +81,16 @@ static int driver_open(struct inode *device_file, struct file *instance)
 static int driver_close(struct inode *device_file, struct file *instance)
 {
     printk("%s mytest_driver - close was called", __FUNCTION__);
-    return 0;   
+    return 0;
 }
 
-
-
-
-
-struct file_operations fops = 
-{
-    .owner=THIS_MODULE,
-    .open=driver_open,
-    .release=driver_close,
-    .read=driver_read,
-    .write=driver_write
-};
-
+struct file_operations fops =
+    {
+        .owner = THIS_MODULE,
+        .open = driver_open,
+        .release = driver_close,
+        .read = driver_read,
+        .write = driver_write};
 
 static int __init hellodriver_init(void)
 {
@@ -124,10 +98,9 @@ static int __init hellodriver_init(void)
     printk("Hello Kernel\n");
     // 1-allocate device number
     retval = alloc_chrdev_region(&device_number, 0, 1, "test_devicenumber");
-    if(retval == 0)
+    if (retval == 0)
     {
-        printk("%s retval=0 - registered device Major number: %d, Minor Number: %d\n", __FUNCTION__,MAJOR(device_number), MINOR(device_number));
-
+        printk("%s retval=0 - registered device Major number: %d, Minor Number: %d\n", __FUNCTION__, MAJOR(device_number), MINOR(device_number));
     }
     else
     {
@@ -137,47 +110,29 @@ static int __init hellodriver_init(void)
     // 2- define driver character or block or network
     cdev_init(&st_characterDevice, &fops);
     retval = cdev_add(&st_characterDevice, device_number, 1);
-    if(retval != 0)
+    if (retval != 0)
     {
         printk("Registering the device to the kernel failed!\n");
         /* In case of failure, you have to delete everything made before */
         goto CHARACTER_ERROR;
-
     }
     // 3- generate file (class - device)
-    if( (myClass = class_create(THIS_MODULE, "test_class")) == NULL )
+    if ((myClass = class_create(THIS_MODULE, "test_class")) == NULL)
     {
         printk("Device class can not be created!\n");
         /* In case of failure, you have to delete everything made before */
         goto CLASS_ERROR;
     }
     myDevice = device_create(myClass, NULL, device_number, NULL, "test_file");
-    if(myDevice == NULL)
+    if (myDevice == NULL)
     {
         printk("Device can not be created!\n");
         /* In case of failure, you have to delete everything made before */
         goto DEVICE_ERROR;
-
     }
     printk("Device Driver is created successfully !\n");
-
-    if(gpio_request(LED_PIN, "rpi_gpio_2"))
-    {
-        printk("Cannot allocate GPIO2\n");
-        goto GPIO_REQUEST_ERROR;
-    }
-
-    if(gpio_direction_output(LED_PIN, 0))
-    {
-        printk("Cannot set the pin direction to be output\n");
-        goto GPIO_DIR_ERROR;
-    }
     return 0;
-    
-GPIO_DIR_ERROR:
-    gpio_free(LED_PIN);
-GPIO_REQUEST_ERROR:
-    device_destroy(myClass, device_number);
+
 DEVICE_ERROR:
     class_destroy(myClass);
 CLASS_ERROR:
@@ -187,12 +142,8 @@ CHARACTER_ERROR:
     return -1;
 }
 
-
-
 static void __exit hellodriver_exit(void)
 {
-    gpio_set_value(LED_PIN, 0);
-    gpio_free(LED_PIN);
     cdev_del(&st_characterDevice);
     device_destroy(myClass, device_number);
     class_destroy(myClass);
