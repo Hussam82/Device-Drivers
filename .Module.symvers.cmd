9b1af0bc2349cdc924945052c967d5a868c3fd27
Hussam Ali <81903080+Hussam82@users.noreply.github.com>
2023-04-03 15:28:02 +0200

    Delete .Module.symvers.cmd

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/hussam/Desktop/Device-Drivers/Module.symvers := sed 's/ko$$/o/' /home/hussam/Desktop/Device-Drivers/modules.order | scripts/mod/modpost -m -a  -o /home/hussam/Desktop/Device-Drivers/Module.symvers -e -i Module.symvers   -T -
