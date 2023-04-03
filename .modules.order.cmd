a3d01b28480abe26821e8df7dd9a4f9582340a18
Hussam Ali <81903080+Hussam82@users.noreply.github.com>
2023-04-03 15:28:31 +0200

    Delete .modules.order.cmd

--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/hussam/Desktop/Device-Drivers/modules.order := {   echo /home/hussam/Desktop/Device-Drivers/hello.ko; :; } | awk '!x[$$0]++' - > /home/hussam/Desktop/Device-Drivers/modules.order
