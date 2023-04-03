a20d9224996f1dc3157bd299612258579954e68b
Hussam Ali <81903080+Hussam82@users.noreply.github.com>
2023-04-03 15:28:14 +0200

    Delete .hello.mod.cmd

--- .hello.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/hussam/Desktop/Device-Drivers/hello.mod := printf '%s\n'   hello.o | awk '!x[$$0]++ { print("/home/hussam/Desktop/Device-Drivers/"$$0) }' > /home/hussam/Desktop/Device-Drivers/hello.mod
